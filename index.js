76eb4464b37f1107adb6f24a093abe91b09ffeae
Michael Heuberger <michael.heuberger@binarykitchen.com>
2018-03-06 17:18:44 +1300

    bump packages, standardjs and fix travis

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var toBuffer       = require('typedarray-to-buffer'),
-    isFloat32Array = require('validate.io-float32array')
+const toBuffer = require('typedarray-to-buffer')
+const isFloat32Array = require('validate.io-float32array')
 
-module.exports = function(float32Array) {
+module.exports = function (float32Array) {
+  if (!float32Array) {
+    throw new Error('A Float32Array parameter is missing.')
+  }
 
-    if (!float32Array)
-        throw new Error('A Float32Array parameter is missing.')
+  if (!isFloat32Array(float32Array)) {
+    throw new Error('The parameter is not a Float32Array.')
+  }
 
-    if (!isFloat32Array(float32Array))
-        throw new Error('The parameter is not a Float32Array.')
+  this.toBuffer = function () {
+    const l = float32Array.length
+    const arr = new Int16Array(l)
+    var i
 
-    this.toBuffer = function() {
-
-        var l   = float32Array.length,
-            arr = new Int16Array(l),
-            i
-
-        for (i = 0; i < l; i++) {
-            arr[i] = Math.min(1, float32Array[i]) * 0x7FFF
-        }
-
-        return toBuffer(arr)
+    for (i = 0; i < l; i++) {
+      arr[i] = Math.min(1, float32Array[i]) * 0x7FFF
     }
+
+    return toBuffer(arr)
+  }
 }
