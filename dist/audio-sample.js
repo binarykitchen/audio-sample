096aeefaf01ef35c3f2357ed9df0dec62238c2ba
Michael Heuberger <michael.heuberger@binarykitchen.com>
2024-08-05 13:51:24 +1200

    bump version

--- dist/audio-sample.js
+++
@@ -1,2 +0,0 @@
-import r from"typedarray-to-buffer";class t{constructor(r){this.float32Array=void 0,this.float32Array=r}toBuffer(){const t=new Int16Array(this.float32Array.length);return this.float32Array.forEach((r,a)=>{t[a]=32767*Math.min(1,r)}),r(t)}}export{t as default};
-//# sourceMappingURL=audio-sample.js.map
